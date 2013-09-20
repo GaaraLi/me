d98d228cb4e0f268d1943cd7bc3187119125b675
gaara <gaara@gaara-H34.(none)>
2013-09-20 16:22:50 +0800

    fix redis

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,5 @@
 Me::Application.configure do
+  ENV["REDISTOGO_URL"] = 'redis://username:password@my.host:6389'
   # Settings specified here will take precedence over those in config/application.rb
 
   # In the development environment your application's code is reloaded on
